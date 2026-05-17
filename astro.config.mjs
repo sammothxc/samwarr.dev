f0feba78d53d1c76d0420c238ef256819f5d8f1d
sammothxc <sam.warr.tx@gmail.com>
2026-05-16 21:02:01 -0600

    fix trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://samwarr.dev',
+  trailingSlash: 'never',
   integrations: [sitemap()],
   build: {
+    format: 'file',
     inlineStylesheets: 'auto',
   },
 });
